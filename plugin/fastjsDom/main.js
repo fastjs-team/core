4b9f62984529ca20c7047e98d68ea4a5d5aac9eb
dy-xiaodong <xiaodong@indouyin.cn>
2022-10-21 21:30:53 +0800

    Optimize code structure Update fastjsDomList

--- plugin/fastjsDom/main.js
+++ plugin/fastjsDom/main.js
@@ -1,15 +1,14 @@
-import dataEdit from "./dataEdit";
-import domEdit from "./domEdit";
-import event from "./event";
-
 const main = function (el) {
     let _this = {
         _el: el,
         construct: "fastjsDom"
     }
-    _this = Object.assign(_this, dataEdit);
-    _this = Object.assign(_this, domEdit);
-    _this = Object.assign(_this, event);
+
+    // import methods
+    _this = Object.assign(_this, import("./dataEdit"));
+    _this = Object.assign(_this, import("./domEdit"));
+    _this = Object.assign(_this, import("./event"));
+
     Object.entries(_this).forEach(e => {
         this[e[0]] = e[1];
     })
--- plugin/fastjsDomList/main.js
+++ plugin/fastjsDomList/main.js
@@ -5,10 +5,17 @@ const main = function (list) {
     list.forEach((v) => {
         domList.push(new fastjsDom(v));
     })
+
     let _this = {
         _list: domList,
         construct: "fastjsDomList"
     }
+
+    // import methods
+    _this = Object.assign(_this, import("./dataEdit"));
+    _this = Object.assign(_this, import("./domEdit"));
+    _this = Object.assign(_this, import("./event"));
+
     domList.forEach((e, key) => {
         this[key] = e;
     })
