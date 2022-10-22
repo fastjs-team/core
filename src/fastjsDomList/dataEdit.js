f8d5461c16ccd50f23772c21bf9192088008c385
dy-xiaodong <xiaodong@indouyin.cn>
2022-10-22 21:38:15 +0800

    Fix 1 bug in fastjsDomList.dataEdit.getEl - unexpected error output

--- src/fastjsDomList/dataEdit.js
+++ src/fastjsDomList/dataEdit.js
@@ -1,18 +1,45 @@
+import _dev from "../dev";
+
 export default _e => {
     return {
         toArray() {
             return _e._list;
         },
-        set(key, val) {
-            _e._list.forEach((e) => {
-                e.set(key, val);
-            })
+        set(val, key) {
+            // dev start
+            if (process.env.NODE_ENV !== 'production') {
+                if (val === undefined)
+                    _dev.newError('fastjsDomList', 'argument val require a value', [
+                        'set(val, key)',
+                        'dataEdit.js',
+                        'fastjsDomList'
+                    ]);
+            }
+            // dev end
+            if (key === undefined)
+                _e.each((e) => {
+                    e.set(val);
+                })
+            else
+                _e.get(key).set(val);
             return _e;
         },
-        get(key, target) {
-            if (key)
-                return _e._list[key || 0]
+        get(target, key) {
             return _e._list[key || 0].get(target);
+        },
+        getEl(key) {
+            // dev start
+            if (process.env.NODE_ENV !== 'production') {
+                // overflow
+                if (key >= _e.length)
+                    return _dev.newWarn('fastjsDomList', 'key is overflow', [
+                        'getEl(key)',
+                        'dataEdit.js',
+                        'fastjsDomList'
+                    ]);
+            }
+            // dev end
+            return _e[key || 0];
         }
     }
 }
