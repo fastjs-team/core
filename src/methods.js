99373851536f123c6bd6b6471153fba3f4b82f2f
dy-xiaodong <xiaodong@indouyin.cn>
2022-10-24 20:03:36 +0800

    Reset methods.js

---
+++ src/methods.js
@@ -0,0 +1,50 @@
+import _dev from "./dev";
+import fastjsDom from "./fastjsDom/fastjsDom";
+import fastjsDomList from "./fastjsDomList/fastjsDomList";
+import config from "./config";
+
+let fastjs = {
+    selecter(el, place = _dev._dom) {
+        let dom = []
+        // if place = fastjsDomList
+        if (place.constructor === fastjsDomList) {
+            place._list.forEach((e) => {
+                e._el.querySelectorAll(el).forEach((v) => {
+                    // check if v is in dom
+                    if (!dom.some((v2) => v2 === v))
+                        dom.push(v);
+                })
+            })
+        } else {
+            dom = place.querySelectorAll(el);
+        }
+        // if last selecter is id
+        let special = el.split(" ")[el.split(" ").length - 1].startsWith("#");
+        // prevent extra forEach
+        if (!special)
+            config.dom.specialDom.forEach((v) => {
+                if (el.startsWith(v))
+                    special = true;
+            })
+        if (special)
+            // -> fastjsDom -> element
+            return new fastjsDom(dom[0]);
+        // -> fastjsDomList -> fastjsDom -> element
+        return new fastjsDomList(dom);
+    },
+    copy(text) {
+        // copy text to clipboard
+        let input = new fastjsDom("input");
+        input.val(text).push();
+        input._el.select();
+        document.execCommand("copy");
+        console.log(input);
+        input.remove();
+    }
+}
+
+let selecter = fastjs.selecter
+let copy = fastjs.copy
+
+export {selecter, copy}
+export default fastjs
