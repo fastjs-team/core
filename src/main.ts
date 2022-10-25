3a77e5917843d43ee3145a5fbc87c254f5716005
dy-xiaodong <xiaodong@indouyin.cn>
2022-10-25 21:21:12 +0800

    Update typescript version - main.ts

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+// modules
+import Ajax from "../modules/ajax/main";
+
+// src
+import FastjsArray from "../src/fastjsArray/fastjsArray";
+import FastjsDom from "../src/fastjsDom/fastjsDom";
+import FastjsDomList from "./fastjsDomList/fastjsDomList";
+
+// main
+import fastjs from "./methods";
+import {selecter, copy} from "./methods";
+
+// export
+export {Ajax, FastjsArray, FastjsDom, FastjsDomList, selecter, copy};
+export default fastjs;
