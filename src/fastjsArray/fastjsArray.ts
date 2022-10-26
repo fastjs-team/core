9728fdb1697f2573659bec466343c09a76e5ca54
dy-xiaodong <xiaodong@indouyin.cn>
2022-10-26 20:57:18 +0800

    Update fastjsArray

---
+++ src/fastjsArray/fastjsArray.ts
@@ -0,0 +1,67 @@
+import _check from "./check";
+import methods from "./methods";
+
+let defaultConfig = {
+    type: "Any",
+    length: null
+}
+
+interface config {
+    type: string | Array<string>,
+    length: number | null
+}
+
+interface proxy {
+    [key: string]: any
+}
+
+class fastjsArray {
+    constructor(array: Array<any>, config: config = defaultConfig) {
+        /*
+        config = {
+            type: <string>::type / <array>::type,
+            length: <number>::max length
+        }
+        */
+
+        const check = _check(this);
+        const effect = () => {
+            this._array.forEach((v: any, k: number) => {
+                this[k] = v;
+            })
+            // check is there an invalid key
+            let i = this._array.length;
+            while (this[i] !== undefined) {
+                delete this[i];
+                i++;
+            }
+        }
+
+        this._array = new Proxy(array, {
+            set: (target: proxy, key: string, value) => {
+                if (!check(value)) return false
+                target[key] = value;
+                effect();
+                return true
+            }
+        });
+        this._config = config;
+
+        // init methods
+        Object.entries(methods).forEach(e => {
+            this[e[0]] = e[1];
+        })
+
+        // init effect
+        effect();
+    }
+
+    _config: config
+    // array = Proxy -> Array
+    _array: proxy
+
+    // methods
+    [key: string]: any
+}
+
+export default fastjsArray
