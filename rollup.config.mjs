43b36ebe8c16891c5c3e26cd41e382a431c29eca
dy-xiaodong <xiaodong@indouyin.cn>
2022-10-28 20:03:16 +0800

    Update rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,21 @@
+// typescript support
+import typescript from '@rollup/plugin-typescript';
+
+
 export default {
     input: "src/main.ts",
     output: [
         {
-            format: "esm",
-            file: "dist/fastjs-next-runtime-esm.ts",
+            format: "es",
+            sourcemap: true,
+            file: "dist/fastjs-next-runtime-esm.js",
         }
+    ],
+    plugins: [
+        typescript({
+            tsconfig: "tsconfig.json",
+            target: "es2015",
+            module: "esnext"
+        })
     ]
 }
