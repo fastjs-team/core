7ee1eea5410f3b7d05bd123e5ac4ccd5fd22516d
dy-xiaodong <xiaodong@indouyin.cn>
2022-10-31 20:34:12 +0800

    Fix 1 bug in Issue #14

--- src/fastjsBind.ts
+++ src/fastjsBind.ts
@@ -8,6 +8,7 @@ interface config {
             bind: string,
         }>
     }
+
     [key: string]: any
 }
 
@@ -23,29 +24,68 @@ interface event {
 
 class fastjsBind {
     constructor(el: fastjsDom, bind: string, key: string | number, object: config = {}, isAttr: boolean = false) {
+        let effect = (target: { [key: string]: any }) => {
+            return new Proxy(target, {
+                set(target: target, p: string, value: any): boolean {
+                    if (_object._event) {
+                        // set value -> pass
+                        target[key] = value;
+                        // do event
+                        if (_object._event[key] && key !== "_event") {
+                            _object._event[key].forEach((e: event) => {
+                                if (e.attr)
+                                    e._el._el.attr(e.bind, value);
+                                else
+                                    e._el._el[e.bind] = value;
+                            })
+                        }
+                    }
+                    return true;
+                }
+            })
+        }
+
         // deep clone object
-        let _object = JSON.parse(JSON.stringify(object));
+        // const deepClone = (source: { [key: string | number]: any }, superCall: boolean = true) => {
+        //     if (typeof source !== 'object' || source == null) {
+        //         return source;
+        //     }
+        //
+        //     let target: { [key: string | number]: any } | Array<any>
+        //
+        //     if (superCall)
+        //         target = effect(source);
+        //     else
+        //         target = Array.isArray(source) ? [] : {};
+        //
+        //     let key: string | number;
+        //     for (key in source) {
+        //         if (Object.prototype.hasOwnProperty.call(source, key)) {
+        //             // if key = string, target != array
+        //             if (typeof key === "string" && !Array.isArray(target)) {
+        //                 // if source[key] = element
+        //                 if (source[key].nodeType) {
+        //                     target[key] = source[key].cloneNode(true);
+        //                 } else if (typeof source[key] === 'object' && source[key] !== null) {
+        //                     target[key] = deepClone(source[key], false);
+        //                 }
+        //             } else if (typeof key === "number" && Array.isArray(target)) {
+        //                 target[key] = source[key];
+        //             }
+        //         }
+        //     }
+        //     return target;
+        // }
+        // deep clone
+        // let _object = deepClone(object);
+        let _object = object;
+
         if (!_object._event) {
             _object._event = {};
+
+            // init effect
+            _object = effect(_object);
         }
-        _object = new Proxy(_object, {
-            set(target: target, p: string, value: any): boolean {
-                if (_object._event) {
-                    // set value -> pass
-                    target[key] = value;
-                    // do event
-                    if (_object._event[key] && key !== "_event") {
-                        _object._event[key].forEach((e: event) => {
-                            if (e.attr)
-                                e._el._el.attr(e.bind, value);
-                            else
-                                e._el._el[e.bind] = value;
-                        })
-                    }
-                }
-                return true;
-            }
-        })
 
         if (_object._event === undefined) return
 
@@ -54,11 +94,14 @@ class fastjsBind {
 
         // find same event & dom
         let same = false;
-        _object._event[key].forEach((e: event) => {
-            if (e._el._el === el._el && e.bind === bind)
+
+        _object._event[key].forEach((e: { [key: string]: any }) => {
+            // is same dom & bind
+            if (e.bind === bind && e._el._el.isEqualNode(el._el))
                 same = true;
-        })
-        if (same) return
+        });
+
+        if (same) return _object;
 
         _object._event[key].push({
             attr: isAttr,
