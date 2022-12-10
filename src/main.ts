c566ae3c2108a584dd958e456d1c9982b4a99f72
DY_XiaoDong <xiaodong@indouyin.cn>
2022-12-10 08:32:31 +0800

    Update src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 // modules
 import FastjsAjax from "../modules/ajax/main";
+import FastjsDate from "../modules/date/main";
 
 // src
-import FastjsArray from "../src/fastjsArray";
+import FastjsArray from "./fastjsArray";
 import FastjsDom from "../src/fastjsDom";
 import FastjsDomList from "./fastjsDomList";
 
@@ -11,5 +12,5 @@ import fastjs from "./methods";
 import {selecter, copy, rand, install} from "./methods";
 
 // export
-export {FastjsAjax, FastjsArray, FastjsDom, FastjsDomList, selecter, copy, rand, install};
+export {FastjsAjax, FastjsDate, FastjsArray, FastjsDom, FastjsDomList, selecter, copy, rand, install};
 export default fastjs;
