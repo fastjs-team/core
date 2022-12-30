9e582984759c9d434c32a53a683a704399fc7aef
DY_XiaoDong <xiaodong@indouyin.cn>
2022-12-30 20:45:57 +0800

    Update FastjsBind

--- src/fastjsBind.ts
+++ src/fastjsBind.ts
@@ -1,93 +1,86 @@
 import fastjsDom from "./fastjsDom";
 
-interface config {
-  _event?: {
-    [key: string]: Array<{
-      attr: boolean;
-      _el: fastjsDom;
-      bind: keyof HTMLElement;
-    }>;
-  };
-
-  [key: string]: any;
-}
-
 interface target {
-  [key: string]: any;
+    _event: {
+        // string = bind key of object(target)
+        [key: string]: Array<event>;
+    };
+
+    [key: string]: any;
 }
 
 interface event {
-  attr: boolean;
-  _el: fastjsDom;
-  bind: keyof HTMLElement;
+    attr: boolean;
+    _el: fastjsDom;
+    bind: keyof HTMLElement;
 }
 
 class fastjsBind {
-  constructor(
-    el: fastjsDom,
-    bind: keyof HTMLElement,
-    key: string | number,
-    object: config = {},
-    isAttr: boolean = false
-  ) {
-    let effect = (target: { [key: string]: any }) => {
-      return new Proxy(target, {
-        set(target: target, p: string, value: any): boolean {
-          if (_object._event) {
-            // set value -> pass
-            target[key] = value;
-            // do event
-            if (_object._event[key] && key !== "_event") {
-              _object._event[key].forEach((e: event) => {
-                if (e.attr) {
-                  e._el.attr(e.bind, value);
-                } else {
-                  e._el.set(e.bind, value);
+    constructor(
+        el: fastjsDom,
+        bind: keyof HTMLElement,
+        key: string | number,
+        object: { [key: string]: any } = {},
+        isAttr: boolean = false
+    ) {
+        let effect = (target: target) => {
+            return new Proxy(target, {
+                set(target: target, p: string, value: any): boolean {
+                    if (target._event) {
+                        // set value -> pass
+                        target[key] = value;
+                        // do event
+                        if (target._event[key] && key !== "_event") {
+                            target._event[key].forEach((e: event) => {
+                                if (e.attr) {
+                                    e._el.attr(e.bind, value);
+                                } else {
+                                    e._el.set(e.bind, value);
+                                }
+                            })
+                        }
+                    }
+                    return true;
                 }
-              });
-            }
-          }
-          return true;
-        },
-      });
-    };
+            })
+        }
 
-    let _object = object;
+        let _object = object;
 
-    if (!_object._event) {
-      _object._event = {};
+        if (!object._event) {
+            object._event = {};
 
-      // init effect
-      _object = effect(_object);
+            // init effect
+            // @ts-ignore
+            object = effect(object);
+        }
+
+        if (!object._event[key])
+            object._event[key] = []
+
+        // find same event & dom
+        let same = false;
+        object._event[key].forEach((e: { [key: string]: any }) => {
+            // is same dom & bind
+            if (e.bind === bind && e._el._el.isEqualNode(el._el)) same = true;
+        });
+        if (same) return object;
+
+        object._event[key].push({
+            attr: isAttr,
+            _el: el,
+            bind: bind
+        });
+
+        // init
+        if (isAttr)
+            el.attr(bind, object[key]);
+        else
+            // @ts-ignore
+            el._el[bind as keyof HTMLElement] = object[key];
+
+        return object;
     }
-
-    if (_object._event === undefined) return;
-
-    if (!_object._event[key]) _object._event[key] = [];
-
-    // find same event & dom
-    let same = false;
-
-    _object._event[key].forEach((e: { [key: string]: any }) => {
-      // is same dom & bind
-      if (e.bind === bind && e._el._el.isEqualNode(el._el)) same = true;
-    });
-
-    if (same) return _object;
-
-    _object._event[key].push({
-      attr: isAttr,
-      _el: el,
-      bind: bind,
-    });
-
-    // init
-    if (isAttr) el.attr(bind, _object[key]);
-    // @ts-ignore
-    else el._el[bind as keyof HTMLElement] = _object[key];
-
-    return _object;
-  }
 }
 
-export default fastjsBind;
+export default fastjsBind
