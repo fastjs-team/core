905585cf824c42459753401a7570358c51a9d83c
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-11-18 12:00:17 +0800

    Update global variables

---
+++ src/global.d.ts
@@ -0,0 +1,7 @@
+declare var __DEV__: boolean
+declare var __VERSION__: string
+declare var __BROWSER__: boolean
+declare var __GLOBAL__: boolean
+declare var __ESM_BUNDLER__: boolean
+declare var __ESM_BROWSER__: boolean
+declare var __NODE_JS__: boolean
