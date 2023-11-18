159e5b680c234b0b66d622c82146172c5fee7ed3
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-11-19 02:36:58 +0800

    #67 - Update automatic test of array

--- test/array.test.js
+++ test/array.test.js
@@ -1,22 +1,21 @@
-// import jest from "jest";
 import {FastjsArray} from "../dist/fastjs.esm.js";
 import {describe, it, expect, jest} from "@jest/globals";
 
 describe("Array create and convert", () => {
   it("Empty Array", () => {
     const result = new FastjsArray().toArray();
+    console.log("Empty Array: ", result);
     expect(result).toEqual([]);
-    console.log("Test result: ", result);
   });
   it("Array with one element", () => {
     const result = new FastjsArray([1]).toArray();
+    console.log("Array with one element: ", result);
     expect(result).toEqual([1]);
-    console.log("Test result: ", result);
   })
   it("Array with two elements", () => {
     const result = new FastjsArray([null, undefined]).toArray();
+    console.log("Array with two elements: ", result);
     expect(result).toEqual([null, undefined]);
-    console.log("Test result: ", result);
   })
 })
 
@@ -25,11 +24,13 @@ describe("Array check", () => {
 
   beforeEach(() => {
     consoleSpy = jest.spyOn(console, 'error');
+    consoleSpy.mockImplementation(() => {});
   });
 
   afterEach(() => {
     consoleSpy.mockRestore();
   });
+
 
   it("Overflow array when create", () => {
     new FastjsArray([1, 2], {
