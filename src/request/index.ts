3f17af2633c7e0140c601ba41b5d9b0e07924479
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-11-20 20:38:32 +0800

    feat(request): body, query, request-scope hooks

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,7 +1,4 @@
-// import _config from "../config";
 import _dev from "../dev";
-
-// import Config from "../config";
 
 interface data {
     [key: string]: any;
@@ -21,21 +18,24 @@ interface requestConfig {
     callback: Function;
     keepalive: boolean;
     keepaliveWait: number;
+    hooks?: {
+        before: (request: FastjsRequest, config: moduleConfig) => boolean;
+        success: (request: FastjsRequest, config: moduleConfig) => boolean;
+        failed: (request: FastjsRequest, config: moduleConfig) => boolean;
+        callback: (
+            request: FastjsRequest,
+            data: {
+                [key: string]: any;
+            }, config: moduleConfig
+        ) => boolean
+    };
+    query?: {
+        [key: string]: any;
+    };
+    body?: {
+        [key: string]: any;
+    } | string;
 }
-
-// interface selectConfig {
-//     timeout?: number;
-//     datatype?: string;
-//     headers?: {
-//         [key: string]: string;
-//     };
-//     shutdown?: boolean;
-//     wait?: number;
-//     failed?: Function;
-//     callback?: Function;
-//     keepalive?: boolean;
-//     keepaliveWait?: number;
-// }
 
 interface moduleConfig {
     timeout: number;
@@ -89,7 +89,6 @@ const moduleConfig: moduleConfig = {
 }
 
 class FastjsRequest {
-    // private readonly construct: string;
     private waitId: number;
     readonly construct: string;
 
@@ -165,14 +164,52 @@ class FastjsRequest {
         return this.send("PATCH", data, "FastjsRequest.patch()");
     }
 
-    send(method: string, data: data = {}, referer: string = "FastjsRequest.send()") {
-        const config = moduleConfig;
-
+    send(method: "GET", data?: data, referer?: string): Promise<any>;
+    send(method: "POST" | "PUT" | "DELETE" | "PATCH", data?: string | data, referer?: string): Promise<any>;
+    send(method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH", data: data | string = {}, referer: string = "FastjsRequest.send()") {
         return new Promise((resolve, reject) => {
-            // method to uppercase
-            method = method.toUpperCase();
+            const hooks = {
+                before: this.config.hooks?.before || moduleConfig.hooks.before || (() => true),
+                success: this.config.hooks?.success || moduleConfig.hooks.success || (() => true),
+                failed: this.config.hooks?.failed || moduleConfig.hooks.failed || (() => true),
+                callback: this.config.hooks?.callback || moduleConfig.hooks.callback || (() => true)
+            };
+            let bodyData: string | null = null;
+            let _bodyData = method === "GET" ? this.config.body : data || this.config.body;
+            if (_bodyData) {
+                if (typeof _bodyData === "string") {
+                    this.config.headers["Content-Type"] = "text/plain";
+                    bodyData = _bodyData;
+
+                    if (__DEV__) {
+                        _dev.warn("fastjs/request", "Sending a string body data is not recommended.", [
+                            `string url: ${this.url}`,
+                            `data Data: ${data}`,
+                            `Partial<config> Config: ${this.config}`,
+                        ]);
+                    }
+                } else {
+                    this.config.headers["Content-Type"] = "application/json";
+                    bodyData = JSON.stringify(_bodyData);
+                }
+
+                if (__DEV__ && method === "GET") {
+                    _dev.warn("fastjs/request", "Sending a body data with GET method should be avoided. (HTTP 1.1)", [
+                        `string url: ${this.url}`,
+                        `data Data: ${data}`,
+                        `Partial<config> Config: ${this.config}`,
+                    ]);
+                }
+            }
+
+            let queryData: data | undefined;
+            if (method === "GET") {
+                queryData = (typeof data === "object" ? data : undefined) || this.config.query;
+            } else {
+                queryData = this.config.query;
+            }
+
             const sendRequest = () => {
-                // if xhr and shutdown is true
                 if (this.xhr !== null && this.config.shutdown) {
                     if (__DEV__) {
                         _dev.warn("fastjs/request", "You've already sent a request with shutdown mode, create a new request or set request.config.shutdown to false.", [
@@ -183,12 +220,12 @@ class FastjsRequest {
                     }
                     return
                 }
-                // create xhr
+
                 let xhr = new XMLHttpRequest();
                 this.xhr = xhr;
                 let url = this.url;
-                if (method === "GET" && Object.keys(this.data).length > 0) {
-                    for (const key in this.data) {
+                if (queryData && Object.keys(queryData).length) {
+                    for (const key in queryData) {
                         if (this.data.hasOwnProperty(key)) {
                             const value = this.data[key];
                             url += `${url.includes("?") ? "&" : "?"}${key}=${encodeURIComponent(value)}`;
@@ -203,9 +240,9 @@ class FastjsRequest {
                 xhr.timeout = this.config.timeout;
                 const fail = (from?: string) => {
                     const result = this.xhr?.status === 0 ?
-                        false : config.handler.parseData(this.xhr?.response, this);
+                        false : moduleConfig.handler.parseData(this.xhr?.response, this);
                     // hook
-                    if (!config.hooks.failed(this, moduleConfig)) return;
+                    if (!hooks.failed(this, moduleConfig)) return;
                     // run failed
                     this.config.failed(this);
                     // if keepalive
@@ -248,9 +285,9 @@ class FastjsRequest {
                     if (xhr.status !== 200) {
                         return void fail("xhr -> onreadystatechange() -> load()");
                     }
-                    const data = config.handler.parseData(xhr.response, this);
-                    if (!config.hooks.success(this, moduleConfig)) return;
-                    if (!config.hooks.callback(this, data, moduleConfig)) return;
+                    const data = moduleConfig.handler.parseData(xhr.response, this);
+                    if (!hooks.success(this, moduleConfig)) return;
+                    if (!hooks.callback(this, data, moduleConfig)) return;
                     this.config.callback(data, this);
                     // if keepalive
                     if (this.config.keepalive) {
@@ -258,12 +295,9 @@ class FastjsRequest {
                     }
                     resolve(data);
                 };
-                let data = "";
-                if (method === "POST") {
-                    data = JSON.stringify(this.data);
-                    xhr.setRequestHeader("Content-Type", "application/json");
-                }
-                xhr.send(data || null);
+
+                if (!hooks.before(this, moduleConfig)) return;
+                xhr.send(bodyData || null);
             };
 
             // if wait
