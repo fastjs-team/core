a35b5e3365ee22dad6106a502943d85c3cd14faf
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-12-20 22:31:25 +0800

    reformat(module/dom): update index.ts
    Signed-off-by: dy-xiaodong2022 <xiaodong@indouyin.cn>

--- src/dom/index.ts
+++ src/dom/index.ts
@@ -1,16 +1,16 @@
 import selector from "./selector";
 import FastjsDom from "./fastjsDom";
 import FastjsDomList from "./fastjsDomList";
-import type {FastjsDomProps} from "./fastjsDom";
+import type {FastjsDomProps} from "./def";
 
-export {
-    selector,
-    FastjsDom,
-    FastjsDomList
-}
 
 export default {
     select: selector,
     newEl: (el: FastjsDom | HTMLElement | Element | string, properties?: FastjsDomProps) => new FastjsDom(el, properties),
     newElList: (list: Array<HTMLElement>) => new FastjsDomList(list)
+}
+export {
+    selector,
+    FastjsDom,
+    FastjsDomList
 }
