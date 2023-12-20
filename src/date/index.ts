616093b1ec3c6f7119f8878f8ec65889ed9d515d
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-12-20 22:30:04 +0800

    update(module/date): update index.ts
    Signed-off-by: dy-xiaodong2022 <xiaodong@indouyin.cn>

--- src/date/index.ts
+++ src/date/index.ts
@@ -1,47 +1,64 @@
 import FastjsDate from "./fastjsDate";
+import type {parseReturn} from "./def";
 
-interface parseReturn {
-    timestamp: number;
-    utcTimestamp: number;
-    format: string;
-    dateString: string;
-    utcDateString: string;
-    date: Date;
-}
-
-const parse = (format: string, time: string | number): parseReturn => {
+/**
+ * @description
+ * Parse a date string or timestamp into a parseReturn object
+ */
+const parse = (time: string | number, format: string = "Y-M-D h:m:s"): parseReturn => {
     const fastjsDateObject = new FastjsDate(format, time);
     const dateString = fastjsDateObject.toString();
     const timestamp = fastjsDateObject.toNumber();
+    const utc = timestamp - (new Date().getTimezoneOffset() * 60 * 1000);
     return {
-        timestamp,
-        utcTimestamp: timestamp - (new Date().getTimezoneOffset() * 60 * 1000),
         format,
+        date: new Date(timestamp),
+        string: dateString,
         dateString,
+        timestamp,
+        utcDate: new Date(utc),
+        utcTimestamp: utc,
         utcDateString: new FastjsDate(format, timestamp - (new Date().getTimezoneOffset() * 60 * 1000)).toString(),
-        date: new Date(timestamp)
     }
 }
 
-const parseTime = (format: string, time: string): parseReturn => {
-    return parse(format, time);
-}
 
-const parseDate = (format: string, date: string): parseReturn => {
-    return parse(format, date);
-}
-
-const string = (format: string, date: number = Date.now()): string => {
-    return parse(format, date).dateString;
-}
-
-const date = (format: string, date: string): Date => {
-    return parse(format, date).date;
-}
-
-const reformat = (format: string, date: string, newFormat: string = "Y-M-D h:m:s"): string => {
-    return new FastjsDate(format, date).toString(newFormat);
-}
+/**
+ * @description
+ * Get a parseReturn object of timestamp or now
+ */
+const parseTime = (time: number, format?: string): parseReturn => parse(time, format);
+/**
+ * @description
+ * Get a parseReturn object of string or now
+ */
+const parseDate = (date: string, format?: string): parseReturn => parse(date, format);
+/**
+ * @description
+ * Get a string of timestamp or now
+ * - For string input, use parse(t, f?) or parseDate(t, f?) instead
+ * - For string to string(reformat), use reformat(t, f, nf?) instead
+ * - For other output, use parse(t, f?) instead
+ */
+const string = (format: string, date: number = Date.now()): string => parse(date, format).string;
+/**
+ * @description
+ * Get a Date object of string or now.
+ * - For number input, use Date(t) instead
+ * - For other output, use parse(t, f?) instead
+ */
+const date = (format: string, date: string): Date => parse(date, format).date;
+/**
+ * @description
+ * Reformat a date string into another format
+ * - For other output, use parse(rf(), f?) instead
+ */
+const reformat = (format: string, date: string, newFormat: string = "Y-M-D h:m:s"): string => new FastjsDate(format, date).toString(newFormat);
+/**
+ * @description
+ * Get a parseReturn object of now
+ */
+const now = (format?: string): parseReturn => parse(Date.now(), format);
 
 export default {
     parse,
@@ -49,7 +66,8 @@ export default {
     parseTime,
     string,
     date,
-    reformat
+    reformat,
+    now
 }
 export {
     FastjsDate
