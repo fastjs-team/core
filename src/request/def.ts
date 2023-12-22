481496710873325dfcfefe70206eb2e11a3b0df6
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-12-22 11:25:07 +0800

    update(module/request): update def.ts
    Signed-off-by: dy-xiaodong2022 <xiaodong@indouyin.cn>

--- src/request/def.ts
+++ src/request/def.ts
@@ -1,38 +1,8 @@
 import _dev from "../dev";
-import FastjsXhrRequest from "./xhr";
+import FastjsRequest from "./xhr";
 
 export interface data {
     [key: string]: any;
-}
-
-export interface xhrRequestConfig {
-    timeout: number;
-    headers: {
-        [key: string]: string;
-    };
-    shutdown: boolean;
-    wait: number;
-    failed: Function;
-    callback: Function;
-    keepalive: boolean;
-    keepaliveWait: number;
-    hooks?: {
-        before: (request: FastjsXhrRequest, config: moduleConfig) => boolean;
-        success: (request: FastjsXhrRequest, config: moduleConfig) => boolean;
-        failed: (request: FastjsXhrRequest, config: moduleConfig) => boolean;
-        callback: (
-            request: FastjsXhrRequest,
-            data: {
-                [key: string]: any;
-            }, config: moduleConfig
-        ) => boolean
-    };
-    query?: {
-        [key: string]: any;
-    };
-    body?: {
-        [key: string]: any;
-    } | string;
 }
 
 export interface requestConfig {
@@ -43,24 +13,44 @@ export interface requestConfig {
     shutdown: boolean;
     wait: number;
     failed: Function;
+    callback: Function;
+    keepalive: boolean;
+    keepaliveWait: number;
+    hooks: {
+        before: (request: FastjsRequest, config: moduleConfig) => boolean;
+        success: (request: FastjsRequest, config: moduleConfig) => boolean;
+        failed: (request: FastjsRequest, config: moduleConfig) => boolean;
+        callback: (
+            request: FastjsRequest,
+            data: {
+                [key: string]: any;
+            }, config: moduleConfig
+        ) => boolean
+    };
+    query: {
+        [key: string]: any;
+    } | string | null;
+    body: {
+        [key: string]: any;
+    } | string | null;
 }
 
 export interface moduleConfig {
     timeout: number;
     hooks: {
-        before: (request: FastjsXhrRequest, config: moduleConfig) => boolean;
-        success: (request: FastjsXhrRequest, config: moduleConfig) => boolean;
-        failed: (request: FastjsXhrRequest, config: moduleConfig) => boolean;
+        before: (request: FastjsRequest, config: moduleConfig) => boolean;
+        success: (request: FastjsRequest, config: moduleConfig) => boolean;
+        failed: (request: FastjsRequest, config: moduleConfig) => boolean;
         callback: (
-            request: FastjsXhrRequest,
+            request: FastjsRequest,
             data: {
                 [key: string]: any;
             }, config: moduleConfig
         ) => boolean
     };
     handler: {
-        parseData: (data: any, request: FastjsXhrRequest) => any;
-        responseCode: (code: number, request: FastjsXhrRequest) => boolean;
+        parseData: (data: any, request: FastjsRequest) => any;
+        responseCode: (code: number, request: FastjsRequest) => boolean;
     }
     ignoreFormatWarning: boolean;
     returnFullResponse: boolean;
