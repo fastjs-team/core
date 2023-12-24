ce5d007a85edb0bea3bc26deafa6ada810cc4068
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-12-25 01:19:25 +0800

    update(request): update def.ts
    Signed-off-by: dy-xiaodong2022 <xiaodong@indouyin.cn>

--- src/request/def.ts
+++ src/request/def.ts
@@ -1,4 +1,6 @@
 import FastjsRequest from "./xhr";
+import FastjsFetchRequest from "./fetch";
+import FastjsXhrRequest from "./xhr";
 
 export interface data {
     [key: string]: any;
@@ -15,17 +17,6 @@ export interface requestConfig {
     callback: Function;
     keepalive: boolean;
     keepaliveWait: number;
-    hooks: {
-        before: (request: FastjsRequest, config: moduleConfig) => boolean;
-        success: (request: FastjsRequest, config: moduleConfig) => boolean;
-        failed: (request: FastjsRequest, config: moduleConfig) => boolean;
-        callback: (
-            request: FastjsRequest,
-            data: {
-                [key: string]: any;
-            }, config: moduleConfig
-        ) => boolean
-    };
     query: {
         [key: string]: any;
     } | string | null;
@@ -34,27 +25,94 @@ export interface requestConfig {
     } | string | null;
 }
 
-export interface moduleConfig {
-    timeout: number;
+export interface xhrRequestConfig extends requestConfig {
     hooks: {
-        before: (request: FastjsRequest, config: moduleConfig) => boolean;
-        success: (request: FastjsRequest, config: moduleConfig) => boolean;
-        failed: (request: FastjsRequest, config: moduleConfig) => boolean;
+        before: (request: FastjsXhrRequest, config: moduleConfig) => boolean;
+        success: (request: FastjsXhrRequest, config: moduleConfig) => boolean;
+        failed: (request: FastjsXhrRequest, config: moduleConfig) => boolean;
         callback: (
-            request: FastjsRequest,
+            request: FastjsXhrRequest,
             data: {
                 [key: string]: any;
             }, config: moduleConfig
         ) => boolean
     };
+}
+
+export interface fetchRequestConfig extends requestConfig {
+    hooks: {
+        before: (request: FastjsFetchRequest, config: moduleConfig) => boolean;
+        success: (request: FastjsFetchRequest, config: moduleConfig) => boolean;
+        failed: (request: FastjsFetchRequest, config: moduleConfig) => boolean;
+        callback: (
+            request: FastjsFetchRequest,
+            data: {
+                [key: string]: any;
+            }, config: moduleConfig
+        ) => boolean
+    };
+}
+
+export interface moduleConfig {
+    timeout: number;
+    hooks: {
+        before: (request: FastjsFetchRequest | FastjsXhrRequest, config: moduleConfig) => boolean;
+        success: (request: FastjsFetchRequest | FastjsXhrRequest, config: moduleConfig) => boolean;
+        failed: (request: FastjsFetchRequest | FastjsXhrRequest, config: moduleConfig) => boolean;
+        callback: (
+            request: FastjsFetchRequest | FastjsXhrRequest,
+            data: {
+                [key: string]: any;
+            }, config: moduleConfig
+        ) => boolean
+    };
+    xhrHooks: {
+        before?: (request: FastjsXhrRequest, config: moduleConfig) => boolean;
+        success?: (request: FastjsXhrRequest, config: moduleConfig) => boolean;
+        failed?: (request: FastjsXhrRequest, config: moduleConfig) => boolean;
+        callback?: (
+            request: FastjsXhrRequest,
+            data: {
+                [key: string]: any;
+            }, config: moduleConfig
+        ) => boolean
+    };
+    fetchHooks: {
+        before?: (request: FastjsFetchRequest, config: moduleConfig) => boolean;
+        success?: (request: FastjsFetchRequest, config: moduleConfig) => boolean;
+        failed?: (request: FastjsFetchRequest, config: moduleConfig) => boolean;
+        callback?: (
+            request: FastjsFetchRequest,
+            data: {
+                [key: string]: any;
+            }, config: moduleConfig
+        ) => boolean
+    }
     handler: {
-        parseData: (data: any, request: FastjsRequest) => any;
-        fetchReturn: (response: Response, request: FastjsRequest) => Promise<object | string>;
-        responseCode: (code: number, request: FastjsRequest) => boolean;
+        parseData: (data: any, request: FastjsXhrRequest) => any;
+        fetchReturn: (response: Response, request: FastjsFetchRequest) => Promise<any>;
+        responseCode: (code: number, request: FastjsXhrRequest | FastjsFetchRequest) => boolean;
     }
     check: {
         ignoreFormatWarning: boolean;
         stringBodyWarning: boolean;
         unrecommendedMethodWarning: boolean;
     }
+}
+
+export interface xhrReturn extends XMLHttpRequest {
+    headers: {
+        [key: string]: string;
+    }
+    data: any;
+    body: any;
+    request: FastjsXhrRequest;
+    xhr: XMLHttpRequest;
+    resend: () => Promise<xhrReturn>;
+}
+
+export interface fetchReturn extends Response {
+    data: any;
+    request: FastjsFetchRequest;
+    resend: () => Promise<fetchReturn>;
 }
