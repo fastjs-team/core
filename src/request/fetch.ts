a4623e5f3242e101befe8ed40de59b470d78f524
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-12-31 23:47:27 +0800

    update(module/request): update fetch event management

--- src/request/fetch.ts
+++ src/request/fetch.ts
@@ -4,14 +4,14 @@ import moduleConfig from "./config";
 import FastjsRequest from "./base";
 import {addQuery} from "./lib";
 
-import type {data, xhrReturn} from "./def";
+import type {data} from "./def";
 import {fetchRequestConfig, fetchReturn} from "./fetch-def";
 
 class FastjsFetchRequest extends FastjsRequest {
     readonly construct: string = "FastjsFetchRequest";
     declare config: fetchRequestConfig;
     private callbacks: {
-        success: ((data: any, response: xhrReturn) => void)[],
+        success: ((data: any, response: fetchReturn) => void)[],
         failed: ((error: Error | any, response: fetchReturn | null) => void)[]
     } = {
         success: [],
@@ -39,18 +39,17 @@ class FastjsFetchRequest extends FastjsRequest {
         return this;
     }
 
-    addFailedCallback(callback: (error: any, response: fetchReturn) => void): this {
+    addFailedCallback(callback: (error: any, response: fetchReturn | null) => void): this {
         this.callbacks.failed.push(callback);
         return this;
     }
 
     then: (callback: (data: any, response: fetchReturn) => void) => this = this.addSuccessCallback;
-    catch: (callback: (error: any, response: fetchReturn) => void) => this = this.addFailedCallback;
+    catch: (callback: (error: any, response: fetchReturn | null) => void) => this = this.addFailedCallback;
 
-
-    send(method: "GET" | "HEAD" | "OPTIONS", data?: data, referer?: string): Promise<fetchReturn>;
-    send(method: "POST" | "PUT" | "DELETE" | "PATCH", data?: string | data, referer?: string): Promise<fetchReturn>;
-    send(method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "HEAD" | "OPTIONS", data: data | string = {}, referer: string = "FastjsFetchRequest.send()"): Promise<fetchReturn> {
+    send(method: "GET" | "HEAD" | "OPTIONS", data?: data, referer?: string): this;
+    send(method: "POST" | "PUT" | "DELETE" | "PATCH", data?: string | data, referer?: string): this;
+    send(method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "HEAD" | "OPTIONS", data: data | string = {}, referer: string = "FastjsFetchRequest.send()"): this {
         const hooks = this.config.hooks
 
         if (__DEV__) {
@@ -86,7 +85,7 @@ class FastjsFetchRequest extends FastjsRequest {
         let queryData: data | string | null = ["GET", "HEAD", "OPTIONS"].includes(method) ?
             data || this.config.query : this.config.query;
 
-        const send = () => {
+        const send = (): void => {
             if (this.request && this.config.shutdown) return
 
             if (!hooks.before(this, moduleConfig)) return this.hookFailed("before");
@@ -110,9 +109,10 @@ class FastjsFetchRequest extends FastjsRequest {
                             response: response,
                             data: data,
                             request: this,
+                            // @ts-ignore
                             resend: () => this.send(method, data, "FastjsFetchRequest.resend()")
                         }
-                        if (this.config.callback) this.config.callback(fullReturn);
+                        if (this.config.callback) this.config.callback(data, fullReturn);
                         this.callbacks.success.forEach((callback) => callback(data, fullReturn));
                     }).catch((error) => {
                         return _dev.warn("fastjs/request", `Failed to parse return, if you are using custom handler(config.handler.fetchReturn), please check your hooks. If not, this may be a bug, check server response and submit an issue to https://github.com/fastjs-team/core/issues.`, [
@@ -138,8 +138,8 @@ class FastjsFetchRequest extends FastjsRequest {
                         console.error(_dev.error("fastjs/request", `Request failed with error`, [
                             "url: " + this.url]))
                     }
-                    if (this.config.failed) this.config.failed(error, this);
-                    this.callbacks.failed.forEach((callback) => callback(error, this));
+                    if (this.config.failed) this.config.failed(error, null);
+                    this.callbacks.failed.forEach((callback) => callback(error, null));
                 });
             } else {
                 this.hookFailed("init");
