a969abdded2eeddf218f2811c6b62b9dc8fa33f4
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-15 05:59:22 +0800

    update(dev): add isDom, isDomList for checking type

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,14 @@
+import type FastjsDom from "./dom/dom";
+import type FastjsDomList from "./dom/dom-list";
+
 export function isUndefined(value: any): value is undefined {
   return value === undefined;
+}
+
+export function isDom(value: any): value is FastjsDom {
+  return value.construct === "FastjsDom";
+}
+
+export function isDomList(value: any): value is FastjsDomList {
+  return value.construct === "FastjsDomList";
 }
