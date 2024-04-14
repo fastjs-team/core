1782dbd49eaaa5b0a0cec4b744745f0379976188
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-14 17:48:46 +0800

    refactor: format index.cjs

--- index.cjs
+++ index.cjs
@@ -1,7 +1,7 @@
-'use strict'
+"use strict";
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/fastjs.cjs.prod.js')
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./dist/fastjs.cjs.prod.js");
 } else {
-  module.exports = require('./dist/fastjs.cjs.js')
+  module.exports = require("./dist/fastjs.cjs.js");
 }
