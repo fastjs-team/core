16429ee4169c28b9147e9cb5f3275e710a6a983b
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-15 05:38:01 +0800

    feat(dom): add selector atom to prevent  circular dependencies

---
+++ src/dom/selector-atom.ts
@@ -0,0 +1,53 @@
+import type FastjsDom from "./dom";
+import type FastjsDomList from "./dom-list";
+
+import _dev from "../dev";
+
+function _selector(
+  selector: string = "body",
+  parent:
+    | Document
+    | HTMLElement
+    | HTMLElement[]
+    | FastjsDom
+    | FastjsDomList = document
+): HTMLElement | HTMLElement[] | null {
+  if (__DEV__) _dev.browserCheck("fastjs/dom/selector");
+
+  const specialStatements = ["body", "head"];
+
+  const result = [];
+
+  // @ts-ignore
+  if (parent?.construct === "FastjsDom") parent = parent._el;
+  // @ts-ignore
+  if (parent?.construct === "FastjsDomList") parent = parent._list;
+
+  Array.isArray(parent)
+    ? parent.forEach((e: HTMLElement) => {
+        result.push(...queryResultToArray(e.querySelectorAll(selector)));
+      })
+    : result.push(...queryResultToArray(parent.querySelectorAll(selector)));
+
+  if (result.length === 0) return null;
+  if (
+    (result[0].id && selector.includes(`#${result[0].id}`)) ||
+    specialStatements.includes(selector)
+  )
+    return result[0];
+  const list: HTMLElement[] = [];
+  result.forEach((e: Element) => {
+    list.push(e as HTMLElement);
+  });
+  return list;
+
+  function queryResultToArray(queryResult: NodeListOf<Element>): HTMLElement[] {
+    const result: HTMLElement[] = [];
+    queryResult.forEach((e: Element) => {
+      result.push(e as HTMLElement);
+    });
+    return result;
+  }
+}
+
+export default _selector;
--- src/dom/selector.ts
+++ src/dom/selector.ts
@@ -1,32 +1,16 @@
-import _dev from "../dev";
 import FastjsDom from "./dom";
 import FastjsDomList from "./dom-list";
+import _selector from "./selector-atom";
+import _dev from "../dev";
 
-function selector(selector: string = "body", parent: Document | HTMLElement | HTMLElement[] = document): FastjsDom | FastjsDomList | null {
-    if (__DEV__)
-        _dev.browserCheck("fastjs/dom/selector")
-
-    const specialStatements = ["body", "head"];
-
-    const result = []
-    Array.isArray(parent) ? parent.forEach((e: HTMLElement) => {
-        result.push(...queryResultToArray(e.querySelectorAll(selector)));
-    }) : result.push(...queryResultToArray(parent.querySelectorAll(selector)));
-    if (result.length === 0) return null;
-    if ((result[0].id && selector.includes(`#${result[0].id}`)) || specialStatements.includes(selector)) return new FastjsDom(result[0] as HTMLElement);
-    const list: HTMLElement[] = [];
-    result.forEach((e: Element) => {
-        list.push(e as HTMLElement);
-    })
-    return new FastjsDomList(list);
-
-    function queryResultToArray(queryResult: NodeListOf<Element>): HTMLElement[] {
-        const result: HTMLElement[] = [];
-        queryResult.forEach((e: Element) => {
-            result.push(e as HTMLElement);
-        })
-        return result;
-    }
+function selector(
+  selector: string = "body",
+  parent: Document | HTMLElement | HTMLElement[] = document
+): FastjsDom | FastjsDomList | null {
+  const result = _selector(selector, parent);
+  if (result instanceof HTMLElement) return new FastjsDom(result);
+  if (Array.isArray(result)) return new FastjsDomList(result);
+  return null;
 }
 
 export default selector;
