6b4e797d07cfdc8271b7abe6cdcffdceac0611d4
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-15 16:38:14 +0800

    ci(test): add unit test for dom/selector

---
+++ test/unit/dom/selector.test.ts
@@ -0,0 +1,44 @@
+/**
+ * @vitest-environment jsdom
+ */
+
+import { assert, test } from "vitest";
+import { dom } from "../../../";
+import { setupDomEnvironment } from "../../utils";
+
+setupDomEnvironment();
+
+test("Select body with selector 'body'", () => {
+  const el = dom.select("body");
+  assert(el?.el() === document.body, "body should be selected");
+});
+
+test("Select body with selector default value null", () => {
+  const el = dom.select();
+  assert(el?.el() === document.body, "body should be selected");
+});
+
+test("Select div with selector 'div'", () => {
+  const el = dom.select("div");
+  assert(el?.el() === document.body.querySelector("div"), "first div should be selected");
+});
+
+test("Select div with selector 'div#root'", () => {
+  const el = dom.select("div#root");
+  assert(el?.el() === document.body.querySelector("div#root"), "root div should be selected");
+})
+
+test("Select div with selector 'div.div-child'", () => {
+  const el = dom.select("div.div-child");
+  assert(el?.el() === document.body.querySelector("div.div-child"), "first div with class div-child should be selected");
+});
+
+test("Select div with selector 'div.div-child' and index 1", () => {
+  const el = dom.select("div.div-child");
+  assert(el?.getDom(1).el() === document.body.querySelectorAll("div.div-child")[1], "second div with class div-child should be selected");
+})
+
+test("Select 2 input with selector 'input[name=textinput]'", () => {
+  const el = dom.select("input[name=textinput]");
+  assert(el?.toArray().length === 2, "3 div with name div-child should be selected");
+})
