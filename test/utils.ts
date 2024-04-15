8824592116895b093dd302698bcb64d532ba75d5
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-15 16:37:57 +0800

    ci(test): add utils.ts and setupDomEnvironment func

---
+++ test/utils.ts
@@ -0,0 +1,11 @@
+export function setupDomEnvironment() {
+  document.body.innerHTML = `
+    <div id="root">
+      <div id="child1" class="div-child"></div>
+      <div id="child2" class="div-child"></div>
+      <div id="child3" class="div-child"></div>
+      <input id="input1" name="textinput" class="input-child" />
+      <input id="input2" name="textinput" class="input-child" />
+    </div>
+  `;
+}
