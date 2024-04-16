14f096c7d746f473016deff67e0dacfe57f90d8f
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-16 11:44:45 +0800

    ci(test): update unit test for dom list

--- test/unit/dom/list.test.ts
+++ test/unit/dom/list.test.ts
@@ -3,28 +3,30 @@
  */
 
 import { assert, test } from "vitest";
-import { dom } from "../../../";
 import { setupDomEnvironment } from "../../utils";
+import type { FastjsDom } from "@/dom";
 
 setupDomEnvironment()
 
+import { dom } from "@/main";
+
 test("Select all div and edit their text", () => {
   const divs = dom.select(".div-child");
-  divs?.text("test").each((el) => {
+  divs?.text("test").each((el: FastjsDom) => {
     assert(el.text() === "test", "div should have text 'test'");
   });
 })
 
 test("Select all input and edit their value", () => {
   const inputs = dom.select(".input-child");
-  inputs?.val("test").each((el) => {
+  inputs?.val("test").each((el: FastjsDom) => {
     assert(el.val() === "test", "input should have value 'test'");
   });
 })
 
 test("Select all div and edit their text using each", () => {
   const divs = dom.select(".div-child");
-  divs?.each((el) => {
+  divs?.each((el: FastjsDom) => {
     el.text("test");
     assert(el.text() === "test", "div should have text 'test'");
   });
@@ -32,14 +34,14 @@ test("Select all div and edit their text using each", () => {
 
 test("Create a list and edit their text", () => {
   const divs = dom.newElList([dom.newEl("div"), dom.newEl("div")]);
-  divs.text("test").each((el) => {
+  divs.text("test").each((el: FastjsDom) => {
     assert(el.text() === "test", "div should have text 'test'");
   });
 })
 
 test("Create a list with real dom and edit their text", () => {
   const divs = dom.newElList([dom.select("#child1"), dom.select("#child2")]);
-  divs.text("newText").each((el) => {
+  divs.text("newText").each((el: FastjsDom) => {
     assert(el.text() === "newText", "div should have text 'test'");
   });
 })
