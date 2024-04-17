e20e349eb4d65878467cff3c1aefb78f63e3c4e4
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-18 07:33:32 +0800

    fix(module): add type exports in entry file #98

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,41 @@
 // all modules
-import date from "./date";
 import dom from "./dom";
+import date from "./date";
 import request from "./request";
 import utils from "./utils/index";
-import {rand, copy} from "./utils/index";
-import {FastjsExpression} from "./base";
 
-import {PushTarget, InsertTarget} from "./dom/def";
+import { rand, copy } from "./utils/index";
+
+import { FastjsExpression } from "./base";
+import { PushTarget, InsertTarget } from "./dom/def";
+
+import type { FastjsDom, FastjsDomList } from "./dom";
+import type { FastjsDate } from "./date";
+import type { FastjsRequest } from "./request";
 
 if (__DEV__) {
-    console.info("You are running fastjs in development mode.\n" +
-        "Make sure to use the production build (*.prod.js) when deploying for production.");
+  console.info(
+    "You are running fastjs in development mode.\n" +
+      "Make sure to use the production build (*.prod.js) when deploying for production."
+  );
 }
 
 // export
 export {
-    date,
-    dom,
-    request,
-    /** @module utils */
-    utils,
-    rand,
-    copy,
-    /** @description enums */
-    PushTarget,
-    InsertTarget,
-    FastjsExpression
+  dom,
+  date,
+  request,
+  /** @module utils */
+  utils,
+  rand,
+  copy,
+  /** @description enums */
+  PushTarget,
+  InsertTarget,
+  FastjsExpression
 };
+export type { FastjsDom, FastjsDate, FastjsDomList, FastjsRequest };
+export type * from "./dom/def";
+export type * from "./dom/css";
+export type * from "./date/def";
+export type * from "./request/config";
