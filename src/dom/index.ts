d139f11529f598bcf9dbe172f7122e5431619a7e
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-21 13:21:02 +0800

    reformat(dom): update api usage and import

--- src/dom/index.ts
+++ src/dom/index.ts
@@ -1,19 +1,20 @@
 import selector from "./selector";
-import FastjsDom from "./dom";
+import { createFastjsDom } from "./dom";
 import { createFastjsDomList } from "./dom-list";
 
 import type { FastjsDomProps } from "./def";
-import type { FastjsDomList } from "./dom-list";
+import type { FastjsDom } from "./dom-types";
+import type { FastjsDomList } from "./dom-list-types";
 
 export default {
   select: selector,
   newEl: (
     el: FastjsDom | HTMLElement | Element | string,
     properties?: FastjsDomProps
-  ) => new FastjsDom(el, properties),
+  ) => createFastjsDom(el, properties),
   newElList: (
     list: Array<FastjsDom | HTMLElement | Element | null | undefined>
   ): FastjsDomList => createFastjsDomList(list)
 };
-export { selector, FastjsDom, createFastjsDomList };
-export type { FastjsDomList };
+export { selector, createFastjsDom, createFastjsDomList };
+export type { FastjsDom, FastjsDomList };
