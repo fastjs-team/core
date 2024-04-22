be53acaf7d1c523319940312c1d6dbdb960e77eb
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-22 20:10:53 +0800

    feat(core): add global variable __TEST__

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import type { FastjsDom, FastjsDomList } from "./dom";
 import type { FastjsDate } from "./date";
 import type { FastjsRequest } from "./request";
 
-if (__DEV__) {
+if (__DEV__ && !__TEST__) {
   console.info(
     "You are running fastjs in development mode.\n" +
       "Make sure to use the production build (*.prod.js) when deploying for production."
@@ -33,9 +33,9 @@ export {
 export type { FastjsDom, FastjsDate, FastjsDomList, FastjsRequest };
 export type * from "./dom/def";
 export type * from "./dom/dom-types";
-export type * from "./dom/dom-list-types"
+export type * from "./dom/dom-list-types";
 export type * from "./date/def";
 export type * from "./date/date-types";
 export type * from "./request/def";
 export type * from "./request/config";
-export type * from "./request/fetch-types"
+export type * from "./request/fetch-types";
