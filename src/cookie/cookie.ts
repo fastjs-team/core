20c9edc6011be1a7f980cf80c565b1da52faecaf
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-23 09:00:12 +0800

    feat(cookie): add cookie base module

---
+++ src/cookie/cookie.ts
@@ -0,0 +1,21 @@
+import { createModule } from "../base";
+import { createMethods } from "./cookie-methods";
+import { FastjsCookie, FastjsCookieAtom } from "./cookie-types";
+
+export default function createFastjsCookie(
+  path?: string,
+  domain?: string
+): FastjsCookie {
+  const moduleAtom = createModule<FastjsCookieAtom>(() => ({
+    construct: "FastjsCookie",
+    path,
+    domain
+  }));
+
+  const module: FastjsCookie = Object.assign(
+    moduleAtom,
+    createMethods(moduleAtom as FastjsCookie)
+  );
+
+  return module;
+}
