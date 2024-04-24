af312bd431303c02feea1cbeb1e82c54be8dfa37
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-24 09:55:25 +0800

    ci(test): add unit test cases for utils function randString

--- test/unit/utils/rand.test.ts
+++ test/unit/utils/rand.test.ts
@@ -12,3 +12,33 @@ test("Random number with decimal", () => {
   expect(num).toBeGreaterThanOrEqual(1);
   expect(num).toBeLessThanOrEqual(10);
 });
+
+test("Random string", () => {
+  const str = utils.randString(10);
+  expect(str).toMatch(/^[a-z]{10}$/);
+});
+
+test("Random string with only uppercase letters", () => {
+  const str = utils.randString(10, { upper: true, lower: false });
+  expect(str).toMatch(/^[A-Z]{10}$/);
+})
+
+test("Random string with all letters", () => {
+  const str = utils.randString(10, { upper: true });
+  expect(str).toMatch(/^[a-zA-Z]{10}$/);
+});
+
+test("Random string with only numbers", () => {
+  const str = utils.randString(10, { number: true, letter: false });
+  expect(str).toMatch(/^\d{10}$/);
+});
+
+test("Random string with custom characters", () => {
+  const str = utils.randString(10, { custom: ["!", "@", "#", "$"] });
+  expect(str).toMatch(/^[a-z!@#$]{10}$/);
+});
+
+test("Random string with only custom characters", () => {
+  const str = utils.randString(10, { custom: "!@#$", letter: false });
+  expect(str).toMatch(/^[!@#$]{10}$/);
+})
