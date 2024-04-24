d34f38f90c36e6d298b3e84c8bd53000cac49a18
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-24 09:02:14 +0800

    refactor: change export method with default

--- src/dom/index.ts
+++ src/dom/index.ts
@@ -2,9 +2,11 @@ import selector from "./selector";
 import { createFastjsDom } from "./dom";
 import { createFastjsDomList } from "./dom-list";
 
-export const select = selector;
-export const newEl = createFastjsDom;
-export const newElList = createFastjsDomList;
+export default {
+  select: selector,
+  newEl: createFastjsDom,
+  newElList: createFastjsDomList
+};
 
 export type * from "./def";
 export type * from "./dom-types";
