a9e7bd46ee5f162bb9c116032623c2cf8c1f941e
DY_XiaoDong <admin@xiaodong.moe>
2024-04-29 18:54:58 +0800

    refactor

--- packages/core/src/utils/dom.ts
+++ packages/core/src/utils/dom.ts
@@ -1,39 +1,5 @@
 import _dev from "../dev";
 
-async function createDomElement(text: string): Promise<HTMLElement> {
-  const { createFastjsDom } = await import("../dom/dom");
-  let input = createFastjsDom("span");
-  input
-    .html(replaceNewLinesAndSpaces(text))
-    .push(document.body, "lastElementChild");
-  return input.el();
-
-  function replaceNewLinesAndSpaces(text: string): string {
-    return text.replace(/\n/g, "<br>").replace(/ /g, "&nbsp;");
-  }
-}
-
-function selectText(element: HTMLElement): Selection | null {
-  const range: Range = document.createRange();
-  range.setStart(element, 0);
-  range.setEnd(element, element.childNodes.length);
-  const selection: Selection | null = window.getSelection();
-  if (!selection) return null;
-  selection.removeAllRanges();
-  selection.addRange(range);
-  return selection;
-}
-
-function copyToClipboard(selection: Selection | null): void {
-  if (!selection) {
-    if (__DEV__) {
-      _dev.warn("fastjs/utils/copy", "selection is null");
-    }
-    return;
-  }
-  document.execCommand("copy");
-}
-
 export async function copy(text: string): Promise<void> {
   try {
     const input = await createDomElement(text);
@@ -48,5 +14,39 @@ export async function copy(text: string): Promise<void> {
         error
       );
     }
+  }
+  
+  async function createDomElement(text: string): Promise<HTMLElement> {
+    const { createFastjsDom } = await import("../dom/dom");
+    let input = createFastjsDom("span");
+    input
+      .html(replaceNewLinesAndSpaces(text))
+      .push(document.body, "lastElementChild");
+    return input.el();
+  
+    function replaceNewLinesAndSpaces(text: string): string {
+      return text.replace(/\n/g, "<br>").replace(/ /g, "&nbsp;");
+    }
+  }
+  
+  function selectText(element: HTMLElement): Selection | null {
+    const range: Range = document.createRange();
+    range.setStart(element, 0);
+    range.setEnd(element, element.childNodes.length);
+    const selection: Selection | null = window.getSelection();
+    if (!selection) return null;
+    selection.removeAllRanges();
+    selection.addRange(range);
+    return selection;
+  }
+  
+  function copyToClipboard(selection: Selection | null): void {
+    if (!selection) {
+      if (__DEV__) {
+        _dev.warn("fastjs/utils/copy", "selection is null");
+      }
+      return;
+    }
+    document.execCommand("copy");
   }
 }
