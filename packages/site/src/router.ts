a0d3cf36630db96a898332c303910748696fc360
xiaodong2008 <admin@xiaodong.moe>
2024-05-05 00:05:34 +0800

    feat(site): add router

---
+++ packages/site/src/router.ts
@@ -0,0 +1,71 @@
+import { dom, FastjsDom } from "jsfast";
+
+export interface Page {
+  path: string;
+  template: string;
+  load: (root: FastjsDom, router: Router) => void;
+  unload?: () => void;
+}
+
+export interface Router {
+  navigate: (isInit?: boolean) => void;
+  location: {
+    path: string;
+    realPath: string;
+  };
+}
+
+let isNavigating = false;
+
+export function setupRouter(root: FastjsDom, pages: Page[], notFound: Page) {
+  root = dom
+    .newEl("div")
+    .set("id", "router")
+    .push(root, "firstElementChild").el;
+
+  const setup = (page: Page, withTransition = true) => {
+    router.location = {
+      path: page.path,
+      realPath: window.location.pathname
+    };
+
+    if (withTransition) {
+      root.addClass("fade").then(() => {
+        page.load(root.html(page.template), router);
+        root.removeClass("fade");
+      }, 300);
+    } else page.load(root.html(page.template), router);
+
+    root.next("a")?.forEach((a: FastjsDom) => {
+      a.addEvent("click", (dom, e) => {
+        console.log("click");
+        e.preventDefault();
+        window.history.pushState({}, "", a.getAttr("href")!);
+        navigate();
+      });
+    });
+  };
+
+  const navigate = (isInit: boolean = false) => {
+    const path = window.location.pathname;
+    const page = pages.find((p) => p.path === path);
+    if (page) setup(page, !isInit);
+    else setup(notFound, false);
+  };
+
+  const router: Router = {
+    navigate: () => {},
+    location: {
+      path: "",
+      realPath: ""
+    }
+  };
+
+  if (isNavigating) return;
+  isNavigating = true;
+
+  window.addEventListener("popstate", () => navigate());
+  navigate(true);
+
+  return router;
+}
