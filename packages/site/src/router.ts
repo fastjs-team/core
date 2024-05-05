1986fd526c2ca99152d6b0b90ad739fe9acd91c8
xiaodong2008 <admin@xiaodong.moe>
2024-05-06 03:00:38 +0800

    feat(site): update router

--- packages/site/src/router.ts
+++ packages/site/src/router.ts
@@ -9,6 +9,7 @@ export interface Page {
 
 export interface Router {
   navigate: (isInit?: boolean) => void;
+  render: (root: FastjsDom) => void;
   location: {
     path: string;
     realPath: string;
@@ -17,7 +18,11 @@ export interface Router {
 
 let isNavigating = false;
 
-export function setupRouter(root: FastjsDom, pages: Page[], notFound: Page) {
+export function setupRouter(
+  root: FastjsDom,
+  pages: Page[],
+  notFound: Page
+): Router {
   root = dom.newEl("div").set("id", "router").push(root, "lastElementChild").el;
 
   const setup = (page: Page, withTransition = true) => {
@@ -30,21 +35,31 @@ export function setupRouter(root: FastjsDom, pages: Page[], notFound: Page) {
       root.addClass("fade").then(() => {
         page.load(root.html(page.template), router);
         root.removeClass("fade");
+        isNavigating = false;
       }, 300);
-    } else page.load(root.html(page.template), router);
+    } else page.load(root.html(page.template), router), (isNavigating = false);
 
+    resolveRouterLink(root);
+  };
+
+  function resolveRouterLink(root: FastjsDom) {
     root.next("a")?.forEach((a: FastjsDom) => {
+      if (!a.getAttr("href")?.startsWith("/")) return;
       a.addEvent("click", (dom, e) => {
-        console.log("click");
         e.preventDefault();
         window.history.pushState({}, "", a.getAttr("href")!);
         navigate();
       });
     });
-  };
+  }
 
   const navigate = (isInit: boolean = false) => {
+    if (isNavigating) return;
+
     const path = window.location.pathname;
+    if (router.location.realPath === path) return;
+
+    isNavigating = true;
     const page = pages.find((p) => p.path === path);
     if (page) setup(page, !isInit);
     else setup(notFound, false);
@@ -52,14 +67,12 @@ export function setupRouter(root: FastjsDom, pages: Page[], notFound: Page) {
 
   const router: Router = {
     navigate: () => {},
+    render: resolveRouterLink,
     location: {
       path: "",
       realPath: ""
     }
   };
-
-  if (isNavigating) return;
-  isNavigating = true;
 
   window.addEventListener("popstate", () => navigate());
   navigate(true);
