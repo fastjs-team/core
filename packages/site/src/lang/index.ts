e6ecb12b254441549f7222cf38fc8f2ed01308ab
xiaodong2008 <admin@xiaodong.moe>
2024-05-06 03:08:30 +0800

    feat(site): add i18n

---
+++ packages/site/src/lang/index.ts
@@ -0,0 +1,34 @@
+import topbar from "./topbar";
+import { cookie } from "jsfast";
+
+export interface LangComponent {
+  lang: string;
+  label?: string;
+  index: {
+    [key: string]: string;
+  };
+}
+export type LangFile = LangComponent[];
+
+interface I18n {
+  [key: string]: LangComponent;
+}
+
+export function getI18n(): I18n {
+  const fileList = [topbar];
+  const i18n: I18n = {};
+
+  fileList.forEach((file) => {
+    file.forEach((component) => {
+      // Base: en, if the language is not fully translated, use en as the fallback(base language)
+      if (component.lang === getCurrentLang() || component.lang === "en") i18n[component.lang] = component;
+    });
+  });
+
+  return i18n;
+}
+
+export function getCurrentLang() {
+  if (!cookie.get("lang")) cookie.set("lang", "en");
+  return cookie.get("lang");
+}
