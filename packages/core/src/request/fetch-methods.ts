80e551118ea9d969c3e750be7197cff87700fa41
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-05-05 20:07:19 +0800

    refactor(core/request): refactor code

--- packages/core/src/request/fetch-methods.ts
+++ packages/core/src/request/fetch-methods.ts
@@ -10,21 +10,20 @@ import type {
 } from "./def";
 
 export function createMethods(request: FastjsRequest): FastjsRequestAPI {
-  return {
-    send: (method: RequestMethod, data: RequestData = {}, url?: string) => {
-      request.data = Object.assign(request.data, data);
-      return sendRequest(request, method, url);
-    },
-    get: (data?: RequestData, url?: string) => request.send("GET", data, url),
-    post: (data?: RequestData, url?: string) => request.send("POST", data, url),
-    put: (data?: RequestData, url?: string) => request.send("PUT", data, url),
-    delete: (data?: RequestData, url?: string) =>
-      request.send("DELETE", data, url),
-    patch: (data?: RequestData, url?: string) =>
-      request.send("PATCH", data, url),
-    head: (data?: RequestData, url?: string) => request.send("HEAD", data, url),
-    options: (data?: RequestData, url?: string) =>
-      request.send("OPTIONS", data, url),
+  const send = (method: RequestMethod, data: RequestData = {}, url?: string) => {
+    request.data = Object.assign(request.data, data);
+    return sendRequest(request, method, url);
+  };
+
+  const methods: FastjsRequestAPI = {
+    send,
+    get: (data?: RequestData, url?: string) => send("GET", data, url),
+    post: (data?: RequestData, url?: string) => send("POST", data, url),
+    put: (data?: RequestData, url?: string) => send("PUT", data, url),
+    delete: (data?: RequestData, url?: string) => send("DELETE", data, url),
+    patch: (data?: RequestData, url?: string) => send("PATCH", data, url),
+    head: (data?: RequestData, url?: string) => send("HEAD", data, url),
+    options: (data?: RequestData, url?: string) => send("OPTIONS", data, url),
     then: (
       callback: (data: any, response: RequestReturn) => void,
       repeat: boolean = false,
@@ -62,4 +61,6 @@ export function createMethods(request: FastjsRequest): FastjsRequestAPI {
       return request;
     }
   };
+
+  return methods;
 }
