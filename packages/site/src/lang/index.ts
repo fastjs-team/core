346587cf278d169e63214a13c31ac1ca7ff82dd3
xiaodong2008 <admin@xiaodong.moe>
2024-05-06 03:11:29 +0800

    feat(site): update i18n

--- packages/site/src/lang/index.ts
+++ packages/site/src/lang/index.ts
@@ -8,18 +8,22 @@ export interface LangComponent {
     [key: string]: string;
   };
 }
-export type LangFile = LangComponent[];
+export type LangFile = {
+  name: string;
+  list: LangComponent[];
+};
 
 interface I18n {
   [key: string]: LangComponent;
 }
 
-export function getI18n(): I18n {
+export function getI18n(name: string): I18n {
   const fileList = [topbar];
   const i18n: I18n = {};
 
   fileList.forEach((file) => {
-    file.forEach((component) => {
+    if (file.name !== name) return;
+    file.list.forEach((component) => {
       // Base: en, if the language is not fully translated, use en as the fallback(base language)
       if (component.lang === getCurrentLang() || component.lang === "en") i18n[component.lang] = component;
     });
