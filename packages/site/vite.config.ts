2e4b869753e2e53e982a04647b26b2429fc2e580
xiaodong2008 <admin@xiaodong.moe>
2024-05-06 03:00:26 +0800

    build(site): update config files

---
+++ packages/site/vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "/Users/xiaodong/Documents/Program/fastjs-team/core/packages/site/src"
+    }
+  }
+});
