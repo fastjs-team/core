ca4c0fe265d177b5ec8b385d66bf052eb0286dfc
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-05-07 12:27:25 +0800

    ci(core/cookie): add unit tests

--- packages/core/test/unit/cookie/cookie.test.ts
+++ packages/core/test/unit/cookie/cookie.test.ts
@@ -12,10 +12,18 @@ test("Add cookie", () => {
 
 test("Remove cookie", () => {
   cookie.remove("test");
+  cookie.remove("test");
   assert.equal(cookie.get("test"), null);
 });
 
+test("Check if cookie exists", () => {
+  assert.equal(cookie.exists("test"), false);
+  cookie.set("test", "test");
+  assert.equal(cookie.exists("test"), true);
+})
+
 test("Add cookie with expires", () => {
+  cookie.remove("test");
   cookie.set("test", "test", { expires: 1000 });
   assert.equal(cookie.get("test"), "test");
   setTimeout(() => {
