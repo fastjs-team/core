d1c8134a4d203ce221403d5e5a9e009bd21eaf03
xiaodong2008 <admin@xiaodong.moe>
2024-05-07 22:01:33 +0800

    fix(site): fix alias

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -1,9 +1,10 @@
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": "/Users/xiaodong/Documents/Program/fastjs-team/core/packages/site/src"
+      "@": path.resolve(__dirname, "src")
     }
   }
 });
