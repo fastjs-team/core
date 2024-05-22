af160644ca23a99478d1bfa553af8a017be31a1b
xiaodong2008 <admin@xiaodong.moe>
2024-05-22 12:17:45 +0800

    ci(core/request): update unit test

--- packages/core/test/unit/request/request.test.ts
+++ packages/core/test/unit/request/request.test.ts
@@ -1,36 +1,54 @@
+import { addQuery, transformPathParams } from "@/request/lib";
 import { expect, test } from "vitest";
 
-import { addQuery } from "@/request/lib";
 import { request } from "@/main";
 
-test("Get a json response and use", async () => {
+test("request.get", async () => {
   const data = await request.get("https://reqres.in/api/users");
   expect(data.page).toBe(1);
   expect(data.data.length).toBeGreaterThan(0);
 });
 
-test("Send a get request with query", async () => {
+test("request.get: with params", async () => {
   const data = await request.get("https://reqres.in/api/users", {
     page: 2
   });
   expect(data.page).toBe(2);
 });
 
-test("Send a post request", async () => {
-  request
+test("request.post", async () => {
+  await request
     .post("https://reqres.in/api/users", {
       name: "XiaoDong",
       job: "Software Engineer"
     })
     .then((data, req) => {
       expect(req.status).toBe(201);
+      expect(req.request.data).toMatchObject({
+        name: "XiaoDong",
+        job: "Software Engineer"
+      });
     });
 });
 
-test("Send a put request", async () => {
-  request
-    .put("https://reqres.in/api/users/:id", {
-      id: 2,
+test("request.put", async () => {
+  await request
+    .put("https://reqres.in/api/users/2", {
+      name: "XiaoDong",
+      job: "Software Engineer"
+    })
+    .then((data, req) => {
+      expect(req.status).toBe(200);
+      expect(req.request.data).toMatchObject({
+        name: "XiaoDong",
+        job: "Software Engineer"
+      });
+    });
+});
+
+test("request.patch", async () => {
+  await request
+    .patch("https://reqres.in/api/users/", {
       name: "XiaoDong",
       job: "Software Engineer"
     })
@@ -39,40 +57,42 @@ test("Send a put request", async () => {
     });
 });
 
-test("Send a patch request", async () => {
-  request
-    .patch("https://reqres.in/api/users/:id", {
-      id: 2,
-      name: "XiaoDong",
-      job: "Software Engineer"
-    })
-    .then((data, req) => {
-      expect(req.status).toBe(200);
-    });
+test("request.delete", async () => {
+  await request.delete("https://reqres.in/api/users/").then((data, req) => {
+    expect(req.status).toBe(204);
+  });
 });
 
-test("Send a delete request", async () => {
-  request
+test("request.delete: with path params", async () => {
+  await request
     .delete("https://reqres.in/api/users/:id", {
       id: 2
     })
     .then((data, req) => {
       expect(req.status).toBe(204);
+      expect(req.request.request?.url).toMatchInlineSnapshot(
+        `"https://reqres.in/api/users/2"`
+      );
     });
 });
 
-test("Should add query to url", () => {
-  const [url] = addQuery("https://reqres.in/api/users", {
+test("lib.addQuery", () => {
+  const url = addQuery("https://reqres.in/api/users", {
     page: 2
   });
   expect(url).toMatchInlineSnapshot(`"https://reqres.in/api/users?page=2"`);
 
-  const [url2] = addQuery("https://reqres.in/api/users/:id", {
+  const url2 = addQuery("https://reqres.in/api/users", "page=2");
+  expect(url2).toMatchInlineSnapshot(`"https://reqres.in/api/users?page=2"`);
+});
+
+test("lib.transformPathParams", () => {
+  const [url] = transformPathParams("https://reqres.in/api/users/:id", {
     id: 2
   });
-  expect(url2).toMatchInlineSnapshot(`"https://reqres.in/api/users/2"`);
+  expect(url).toMatchInlineSnapshot(`"https://reqres.in/api/users/2"`);
 
-  const [url3] = addQuery("https://reqres.in/api/users/:id/:name", {
+  const [url3] = transformPathParams("https://reqres.in/api/users/:id/:name", {
     id: 2,
     name: "XiaoDong"
   });
