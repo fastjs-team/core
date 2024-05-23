8812d7962a53d79c30751411a78736246b427a2b
xiaodong2008 <admin@xiaodong.moe>
2024-05-23 09:26:22 +0800

    ci(core/dom): update unit test

--- packages/core/test/unit/dom/dom.test.ts
+++ packages/core/test/unit/dom/dom.test.ts
@@ -3,17 +3,18 @@
  */
 
 import { assert, test } from "vitest";
-import { setupDomEnvironment } from "../../utils";
+
 import { dom } from "@/main";
+import { setupDomEnvironment } from "../../utils";
 
 setupDomEnvironment();
 
-test("Create a FastjsDom element normally", () => {
+test("dom.newEl", () => {
   const el = dom.newEl("div");
   assert(el.el().tagName === "DIV", "div should be created");
 });
 
-test("Create a FastjsDom element with style", () => {
+test("dom.newEl: with style object", () => {
   const el = dom.newEl("div", {
     css: {
       color: "red"
@@ -22,17 +23,28 @@ test("Create a FastjsDom element with style", () => {
   assert(el.el().style.color === "red", "div should be created with red color");
 });
 
-test("Create a FastjsDom element with class", () => {
+test("dom.newEl: with style string", () => {
   const el = dom.newEl("div", {
-    class: "test"
+    css: "color: red;"
+  });
+  assert(el.el().style.color === "red", "div should be created with red color");
+})
+
+test("dom.newEl: with class", () => {
+  const el = dom.newEl("div", {
+    class: "test1 test2"
   });
   assert(
-    el.getClass().includes("test"),
-    "div should be created with test class"
+    el.getClass().includes("test1"),
+    "div should be created with test1 class"
+  );
+  assert(
+    el.getClass().includes("test2"),
+    "div should be created with test2 class"
   );
 });
 
-test("Create a FastjsDom element with class list", () => {
+test("dom.newEl: with class array", () => {
   const el = dom.newEl("div", {
     class: ["test1", "test2"]
   });
@@ -46,14 +58,15 @@ test("Create a FastjsDom element with class list", () => {
   );
 });
 
-test("Create a FastjsDom element with text", () => {
+test("dom.newEl: with text", () => {
   const el = dom.newEl("div", {
-    text: "test"
+    text: "<span>test</span>"
   });
-  assert(el.text() === "test", "div should be created with text 'test'");
+  assert(el.next("span") === null, "span should not be created");
+  assert(el.text().indexOf("test"), "div should be created with text 'test'");
 });
 
-test("Create a FastjsDom element with html", () => {
+test("dom.newEl: with html", () => {
   const el = dom.newEl("div", {
     html: "<span>test</span>"
   });
@@ -67,30 +80,21 @@ test("Create a FastjsDom element with html", () => {
   );
 });
 
-test("Create a FastjsDom element with input value", () => {
+test("dom.newEl: input with value", () => {
   const el = dom.newEl("input", {
-    value: "test"
+    val: "test"
   });
   assert(el.val() === "test", "input should be created with value 'test'");
 });
 
-test("Create a FastjsDom element with textarea value", () => {
+test("dom.newEl: textarea with value", () => {
   const el = dom.newEl("textarea", {
-    value: "test"
+    val: "test"
   });
   assert(el.val() === "test", "textarea should be created with value 'test'");
 });
 
-test("Create a FastjsDom element with attribute", () => {
-  const el = dom.newEl("div", {
-    attr: {
-      id: "test"
-    }
-  });
-  assert(el.getAttr("id") === "test", "div should be created with id 'test'");
-});
-
-test("Create a FastjsDom element with multiple attributes", () => {
+test("dom.newEl: with attributes", () => {
   const el = dom.newEl("div", {
     attr: {
       id: "test",
