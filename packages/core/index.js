32cfd9f9f2ac0db45cbe7d4a49437d4bfb30878f
xiaodong2008 <admin@xiaodong.moe>
2024-05-24 19:50:12 +0800

    fix(core): update entry file

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,1 +1,7 @@
-export * from "./index.js";
+"use strict";
+
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./dist/fastjs.esm.prod.js");
+} else {
+  module.exports = require("./dist/fastjs.esm.js");
+}
