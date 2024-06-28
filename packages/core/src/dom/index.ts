d0ee24deb332d01459f2c903345b12c0f2582495
xiaodong2008 <admin@xiaodong.moe>
2024-06-28 20:05:28 +0800

    feat(core/dom): export a selector

--- packages/core/src/dom/index.ts
+++ packages/core/src/dom/index.ts
@@ -1,12 +1,22 @@
 import selector from "./selector";
 import { createFastjsDom } from "./dom";
 import { createFastjsDomList } from "./dom-list";
+import type { FastjsDom } from "./dom-types";
+import type { FastjsDomList } from "./dom-list-types";
 
-export default {
-  select: selector,
-  newEl: createFastjsDom,
-  newElList: createFastjsDomList
+const dom = function <
+  T extends FastjsDom | FastjsDomList | null = FastjsDom | FastjsDomList | null
+>(
+  target: string = "body",
+  parent: Document | HTMLElement | HTMLElement[] = document
+): T {
+  return selector(target, parent);
 };
+dom.select = selector;
+dom.newEl = createFastjsDom;
+dom.newElList = createFastjsDomList;
+
+export default dom;
 
 export type * from "./def";
 export type * from "./dom-types";
